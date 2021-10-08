6047d671da7d79f0c9592253cfc439d1743ebfa5
Olya Kikot <kikotolya1989@gmail.com>
2021-10-08 11:34:48 +0300

    new

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,17 @@
 import s from "./Modal.module.css";
+import { ReactComponent as CloseLogo } from "../../icon-close.svg";
 
 export default function Modal({ src, onCloseModal, handleOverlay }) {
   return (
     <div className={s.Overlay} onClick={handleOverlay}>
       <div className={s.Modal}>
-        <img src={src} alt="" />
+        <img src={src} alt="" width="1200px" height="800px" />
         <button
           className={s.closeModal}
           type="button"
           onClick={() => onCloseModal()}
         >
-          X
+          <CloseLogo className={s.CloseLogo} />
         </button>
       </div>
     </div>
